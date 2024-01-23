5e7177b8782d78af344aca7481504cd5bb7d095b
Pranjal Gupta <prgu8117@aesd-students.colorado.edu>
2024-01-23 00:08:17 +0000

    Added Comments

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
+#checking if there are exact two arguments or not
 if [ "$#" -ne 2 ]; then
 	echo "Either Directory or String is not specified"
 	exit 1
 fi
 
+#Checking if the first argument is directory of not
 if [ ! -d "$1" ]; then
 	echo "First argument is not a directory"
 	exit 1
 fi
 
+#checking if the string is empty or non-empty
 if [ ! -n "$2" ]; then 
 	echo "Second Argument is empty string"
 	exit 1
 fi
 
-
+#calculating the files inside the given the given directory
 myvar_x=$( ls "$1" | wc -l )
 
+#counting the lines in the given directory which includes the files with given string
 myvar_y=$( grep -r "$2" "$1" | wc -l )
 
 echo "The number of files are $myvar_x and the number of matching lines are $myvar_y"
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+#checking the number of arguments which should be 2
 if [ "$#" -ne 2 ]; then
 	echo "Either Directory or String is not specified"
 	exit 1
 fi
 
+#checking if the given string is non empty or empty
 if [ ! -n "$2" ]; then 
 	echo "Second Argument $2 is empty string"
 	exit 1
 fi
 
+#making the directory by extracting the directory from the complete pathname which include the file name also
 mkdir -p "$(dirname "$1")"
 
+#writing the string into the desired file by creating first if it does not exist
 echo "$2" > "$1"
 
+#checking if the file was created or not
 if [ ! -e "$1" ]; then
 	echo "The file $1 could not be created"
 	exit 1
